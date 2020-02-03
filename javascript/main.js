56098e1bd135b618518fc17f075873becc3e4ce0
panonis <pancicsrdjan@yahoo.com>
2020-02-03 18:16:38 +0100

    First commit

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+// ============== portfolio category buttons ===========
+
+$(document).ready(function () {
+  $(".button").click(function () { 
+    var name = $(this).attr("data-filter");
+    if(name == "all"){
+      $(".filter").show("2000");
+    }
+    else{
+      $(".filter").not("."+name).hide("2000");
+      $(".filter").filter("."+name).show("2000");
+    }
+    
+  });
+  $(".navigation a").click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+  })
+});
+
+
